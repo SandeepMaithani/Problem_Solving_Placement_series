2c6d9b4bc6643e11e3cf309e43672e8bc26d0f86
SandeepMaithani <sandeep1137@gmail.com>
2020-09-28 23:58:45 +0530

    Inversion of array

---
+++ Solutions/Inversion of Array (Using Merge Sort).cpp
@@ -0,0 +1,89 @@
+/*
+
+Method 1 : Using Enhance Merge Sort                            verdict : Accepted
+
+Complexity Analysis:
+
+Time Complexity: O(N*log(N)), The algorithm used is divide and conquer, So in each level one full array traversal is needed and there are log(N) levels so the time complexity is O(N*log(N)).
+
+Space Compelxity: O(n), space require to store the Temporary array.
+
+*/
+
+
+
+int merge(vector<int> &arr, vector<int> &temp, int left, int mid, int right) {
+	int first, second, third;
+	long invCount = 0;
+
+	first = left;
+	second = mid;
+	third = left;
+
+	while ((first <= mid - 1) && (second <= right)) {
+		if (arr[first] <= arr[second]) {
+			temp[third] = arr[first];
+			third++;
+			first++;
+		}
+		else {
+			temp[third] = arr[second];
+			third++;
+			second++;
+			invCount += (mid - first);
+		}
+	}
+
+	while (first <= mid - 1) {
+		temp[third] = arr[first];
+		third++;
+		first++;
+	}
+
+	while (second <= right) {
+		temp[third] = arr[second];
+		third++;
+		second++;
+	}
+
+	for (int i = left; i <= right; i++) {
+		arr[i] = temp[i];
+	}
+
+	return invCount;
+
+}
+
+long countHelper(vector<int> &arr, vector<int> &temp, int left, int right) {
+	int mid;
+	long invCount = 0;
+
+	if (left < right) {
+		mid = left + (right - left) / 2;
+
+		invCount += countHelper(arr, temp, left, mid);
+		invCount += countHelper(arr, temp, mid + 1, right);
+
+		invCount += merge(arr, temp, left, mid + 1, right);
+	}
+
+	return invCount;
+}
+
+long countInversions(vector<int> arr) {
+	vector<int>temp(arr.size());
+	return countHelper(arr, temp, 0, arr.size() - 1);
+}
+
+
+/*
+
+Method 1 : Brute force                          verdict : TLE
+
+Complexity Analysis:
+
+Time Complexity: O(n^2), Two nested loops are needed to traverse the array from start to end so the Time complexity is O(n^2).
+
+Space Compelxity:O(1), No extra space is required.
+
+*/
